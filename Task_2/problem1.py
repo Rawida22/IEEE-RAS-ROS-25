37dbbfb7ff4d9d26febeef241e2198d2df67b266
Rawida22 <rwidahany320@gmail.com>
2025-03-27 12:30:38 -0700

    problem1.py

--- Task_2/problem1.py
+++ Task_2/problem1.py
@@ -1,25 +1,15 @@
 
-#num = input("Enter your Numbers(-1 to stop):")
-#numbers=list(map(int,num.split()))
-
-#for x in num:
-  #if x == -1:
- #  break
-
-#print(f"{max(numbers),{min(numbers)}}")
 numbers = []
 print("Enter numbers one by one (enter -1 to stop):")
 
 while True:
     user_input = input("> ")
-    
-    # Check first if input is -1
     if user_input.strip() == "-1":
         break
     num = int(user_input)
     numbers.append(num)
 
 if numbers:
-    print(f"Largest number: {max(numbers)}, Smallest number: {min(numbers)}")
+    print(f" {max(numbers)}, {min(numbers)}")
 else:
     print("No valid numbers were entered")
